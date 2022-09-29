f9420cad847e07892941da69567b19cfa99c74f0
Cxderzz <skell264@eq.edu.au>
2022-09-29 23:15:29 +1000

    changes

--- index.js
+++ index.js
@@ -4,23 +4,45 @@ const {
     GatewayIntentBits,
     AttachmentBuilder,
     EmbedBuilder,
+    ConnectionVisibility,
 } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
     console.log("Ready!");
 });
 
-client.on("message", (message) => {
-    var interval = setInterval(() => {
-        message.channel
-            .send("Hello") // increment the variable
-            .catch(console.error); // add error handling here
-    }, 1000);
+client.on("messageCreate", (message) => {
+    if (
+        message.content == "start here 123123" &&
+        message.member.id !== "1024334740444172339"
+    ) {
+        setInterval(() => {
+            const day = new Date().getUTCDay();
+            if (day !== 4) return;
+            const embedFriday = new EmbedBuilder()
+                .setDescription("It's Friday In California.")
+                .setTitle("Hey!")
+                .setColor("DarkPurple");
+            const file = new AttachmentBuilder("./friday.mp4");
+            message.channel.send({
+                content: "Hello",
+                embeds: [embedFriday],
+                files: [file],
+            });
+        }, 86400 * 1000);
+    }
+    //  // increment the variable
 });
 
 client.on("interactionCreate", async (interaction) => {
